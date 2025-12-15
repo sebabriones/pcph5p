985d82530c6e79e0f1d3cca90ac40f3f836863eb
sebabriones <s.briones96@gmail.com>
2025-12-14 23:45:39 -0300

    14/12/25

---
+++ standalone/resources/m1/l3/m1_l3_e6/H5P.Instructions-1.0/instructions.js
@@ -0,0 +1,52 @@
+jQuery(document).ready(()=>{
+    /////////////////////////INTRO CON INSTRUCCIONES////////////////////////////
+    $('body').prepend(`<div class="intro">
+                            <div class="text-content">
+                                <div class="intro-text"><strong>Laberinto Blockly</strong><br>Ayuda al personaje (flecha) a llegar al punto B utilizando los bloques de la columna gris. Arrástralos hacia la zona blanca punteada para apilarlos de arriba abajo, siguiendo la lógica de pasos necesarios. Al finalizar, presiona 'INICIAR' para ver el resultado.</div>
+                                <a class="intro-btn">Comenzar</a>
+                            </div>
+                        </div>`);
+
+    $('.intro-btn').on('click',(e)=>{
+        $('.intro').animate({
+                marginTop:`-${$('.h5p-container').outerHeight()}px`,
+        }, 1000);
+
+        setTimeout(() => {
+            $('.intro').css({'display':'none'});
+        }, 1000);
+    });
+
+    /////////////////////////PESTAÑA CON INSTRUCCIONES PARA H5P/////////////////////////////
+
+    $('.h5p-content').prepend(`<div class="custom-instructions">
+                                    <div class="tab-container">
+                                        <div id="tab" class="tab">
+                                            <div class="tab-content">
+                                                <div class="instructions-text"><strong>Laberinto Blockly</strong><br>Ayuda al personaje (flecha) a llegar al punto B utilizando los bloques de la columna gris. Arrástralos hacia la zona blanca punteada para apilarlos de arriba abajo, siguiendo la lógica de pasos necesarios. Al finalizar, presiona 'INICIAR' para ver el resultado.</div>
+                                            </div>
+                                            <a class="instructions-btn" href="">Instrucciones</a>
+                                        </div>
+                                    </div>
+                                </div>`);
+
+    $('.instructions-btn').on('click',(e)=>{
+        e.preventDefault();
+
+        if(parseInt($('.custom-instructions').css('margin-top').split("px")[0]) == 0){
+            $('.custom-instructions').animate({
+                marginTop:`-${$('.tab-content').outerHeight()}px`,
+            }, 1000);
+        }else if(parseInt($('.custom-instructions').css('margin-top').split("px")[0]) < 0){
+            $('.custom-instructions').animate({
+                marginTop:'0',
+            }, 1000);
+        }
+    });
+
+    setTimeout(() => {
+        $('.custom-instructions').animate({
+            marginTop:`-${$('.tab-content').outerHeight()}px`,
+        }, 1000);
+    }, 500);
+});
