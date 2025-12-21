b2bf48136aee935489d61329ac825524a9daacd4
sebabriones <s.briones96@gmail.com>
2025-12-21 02:57:03 -0300

    20/12/25

--- standalone/h5p.php
+++ standalone/h5p.php
@@ -7,7 +7,8 @@
     <link rel="stylesheet" media="all" href="css/main.css">
 </head>
     <body style="background:#324253;">
-        <div id="h5p-container" style="width:60%;text-align:center;padding-left: 20vw;padding-top: 5vh;"></div>
+        <!--<div id="h5p-container" style="width:60%;text-align:center;padding-left: 20vw;padding-top: 5vh;"></div>-->
+        <div id="h5p-container"></div>
 
         <!--<script type="text/javascript" src="assets/jquery-3.7.1.min.js"></script>-->
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
