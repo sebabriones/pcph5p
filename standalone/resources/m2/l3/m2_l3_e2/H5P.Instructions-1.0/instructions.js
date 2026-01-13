224af9878aededf646093e7be89d345ed4decd72
sebabriones <s.briones96@gmail.com>
2026-01-13 17:12:01 -0300

    Add: M2L3 Content

---
+++ standalone/resources/m2/l3/m2_l3_e2/H5P.Instructions-1.0/instructions.js
@@ -0,0 +1,52 @@
+jQuery(document).ready(()=>{
+    /////////////////////////INTRO CON INSTRUCCIONES////////////////////////////
+    $('body').prepend(`<div class="intro">
+                            <div class="text-content">
+                                <div class="intro-text">Observa atentamente la escena. Mediante un ejercicio de abstracción, ubica los <strong>3 elementos mínimos necesarios</strong> para que esta habitación pueda definirse como un <strong>dormitorio</strong> (1° nivel de abstracción).<br>Toma y arrastra hacia la zona de descarte (dormitorio) los elementos que no son indispensables.</div>
+                                <a class="intro-btn">Comenzar</a>
+                            </div>
+                        </div>`);
+
+    $('.intro-btn').on('click',(e)=>{
+        $('.intro').animate({
+                marginTop:`-${$('.h5p-container').outerHeight()}px`,
+        }, 1000);
+
+        setTimeout(() => {
+            $('.intro').css({'display':'none'});
+        }, 1000);
+    });
+
+    /////////////////////////PESTAÑA CON INSTRUCCIONES PARA H5P/////////////////////////////
+
+    $('.h5p-content').prepend(`<div class="custom-instructions">
+                                    <div class="tab-container">
+                                        <div id="tab" class="tab">
+                                            <div class="tab-content">
+                                                <div class="instructions-text">Observa atentamente la escena. Mediante un ejercicio de abstracción, ubica los <strong>3 elementos mínimos necesarios</strong> para que esta habitación pueda definirse como un <strong>dormitorio</strong> (1° nivel de abstracción).<br>Toma y arrastra hacia la zona de descarte (dormitorio) los elementos que no son indispensables.</div>
+                                            </div>
+                                            <a class="instructions-btn" href="">Instrucciones</a>
+                                        </div>
+                                    </div>
+                                </div>`);
+
+    $('.instructions-btn').on('click',(e)=>{
+        e.preventDefault();
+
+        if(parseInt($('.custom-instructions').css('margin-top').split("px")[0]) == 0){
+            $('.custom-instructions').animate({
+                marginTop:`-${$('.tab-content').outerHeight()}px`,
+            }, 1000);
+        }else if(parseInt($('.custom-instructions').css('margin-top').split("px")[0]) < 0){
+            $('.custom-instructions').animate({
+                marginTop:'0',
+            }, 1000);
+        }
+    });
+
+    setTimeout(() => {
+        $('.custom-instructions').animate({
+            marginTop:`-${$('.tab-content').outerHeight()}px`,
+        }, 1000);
+    }, 500);
+});
